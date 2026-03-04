a1cd12fd4e1d4f8b13c7eacef789b2e6244e5b70
severusho <contactseverusho@gmail.com>
2026-03-04 14:02:58 +0300

    Update strings learning file- String transformations and count

--- Learning/strings_learning.py
+++ Learning/strings_learning.py
@@ -5,3 +5,19 @@ if len(password) < 8:
   print("Password must be at least 8 characters long.")
 elif len(password) > 20:
   print("Password is too long.")
+
+#Lesson on how to measure the length of string alues
+text = """ 
+I love coding with python
+python is looking great so far
+I am excited to learn more about python
+python python python python python python python python python python python python python python python python python python!
+
+"""
+print("The string appears", text.count("python"), "times")
+
+#Data transformation using string methods 
+price = "$450,000"
+print (price.replace(",", " ").replace("$", ""))
+phonenumber = "123-456-7890"
+print(phonenumber.replace("-", " "),"We replaced things here successfully")
