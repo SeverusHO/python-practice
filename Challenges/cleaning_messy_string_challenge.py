2f12ed90c7aabd988ff30bc810466935e0383a27
Severus <contact.severusho@gmail.com>
2026-03-08 23:58:02 +0300

    Replaced @ with a

--- Challenges/cleaning_messy_string_challenge.py
+++ Challenges/cleaning_messy_string_challenge.py
@@ -1,4 +1,8 @@
 ##Python Challenge Turn the messy string into a single clean summary with name, role, and age
 # ##"968-Maria, ( Data Engineer );; 27y  "##
-text = "968-Maria, ( Data Engineer );; 27y  "
-print("Name:",text[4:9],"|".strip(),"Role:",text[12:26].strip(),"|" "Age:",text[-5:-3].strip())
+text = "968-Maria, ( D@t@ Engineer );; 27y  "
+
+# print("Name:",text[4:9],"|".strip(),"Role:",text[12:26].strip(),"|" " ", "Age:",text[-5:-3].strip(), )
+
+
+print("Name:",text[4:9],"|".strip(),"Role:",text[12:26].strip().replace("@","a"),"|" " ", "Age:",text[-5:-3].strip())
