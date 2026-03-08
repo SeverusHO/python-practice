56c129aab3060ac13a62cd83733ace70ea968d0e
Severus <contact.severusho@gmail.com>
2026-03-08 22:06:51 +0300

    Indexing and slicing practice

---
+++ Learning/indexing_n_slicing.py
@@ -0,0 +1,38 @@
+Greeting = "Hello World "
+# print (Greeting)
+
+# print (Greeting [0]) #Specific character of string index number (Remember positive indexing starts from 0 while negative starts with -1)
+
+# print (Greeting [-1]) 
+
+# print (Greeting [0:3]) #Start to end range
+
+# print (Greeting [0:]) #Start no end
+
+# print (Greeting [0:11:3]) #Start:End:Skip (:3=yes/no/yes)
+
+# Indexing and Slicing
+date = "8-03-2026"
+# print(date)
+
+# Extract the day
+print(date[0])
+
+#Extract the month
+
+print(date[2:4])
+
+
+# Extract the year
+print (date[5:])
+print (date[-4:])
+
+
+
+
+
+
+
+
+
+
