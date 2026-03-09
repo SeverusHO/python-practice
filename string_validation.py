f8908b523bf5023cee34997f57ff55827b1718e6
Severus <contact.severusho@gmail.com>
2026-03-09 22:41:40 +0300

    Learnt searching in strings and string validation

---
+++ string_validation.py
@@ -0,0 +1,5 @@
+Country= "Italy"
+print(Country.isalpha()) #output is boolean, checks if all characters in the string are alphabetic
+
+phone = "3417412345"
+print(phone.isnumeric()) #output is boolean, checks if all characters in the string are numeric, in this case it will return false because of the "+" and "-" characters
