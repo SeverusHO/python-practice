5c320938fe4d4ed42314b54ea8ac3cf4d670dbce
Severus <contact.severusho@gmail.com>
2026-05-12 22:16:37 +0300

    Learnt data validation

---
+++ Learning/checking_validating_values.py
@@ -0,0 +1,7 @@
+#is Alpha checks if all characters in the string are letters (a-z, A-Z) and there is at least one character. It returns True if all characters are letters and False otherwise.
+country = "Zambia//"
+print(country.isalpha()) #True
+
+#Is Numeric checks if all characters in the string are numeric (0-9) and there is at least one character. It returns True if all characters are numeric and False otherwise.
+phone= "512340982"
+print(phone.isnumeric()) #True 
